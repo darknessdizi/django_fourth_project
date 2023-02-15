007d28f25a6c0f1747104727d0a38f44b420176e
Remezov Dmitriy <darknessdizi@gmail.com>
2023-02-15 19:12:39 +0700

    Проект рабочий, неправильные модели используются

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -6,7 +6,8 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+    students_list = Student.objects.all()
+    context = {'object_list': students_list}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
