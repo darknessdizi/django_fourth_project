982ffa3079d123b0f95664c43f772f7b22c0cca9
Remezov Dmitriy <darknessdizi@gmail.com>
2023-02-15 20:59:26 +0700

    Изменены связи моделей на многие ко многим

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -6,11 +6,7 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    students_list = Student.objects.all()
-    context = {'object_list': students_list}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
-
+    students_list = Student.objects.all().order_by(ordering)
+    context = {'object_list': students_list}
     return render(request, template, context)
