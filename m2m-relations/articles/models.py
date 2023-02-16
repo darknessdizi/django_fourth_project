a91faf8c1bcfd25997243a3619a0e9c051cc6ef7
Remezov Dmitriy <darknessdizi@gmail.com>
2023-02-17 00:59:50 +0700

    Созданы модели типа m2m

--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 
-class Article(models.Model):
+class Tags(models.Model):
+    name = models.CharField(max_length=30)
 
+
+class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+    scopes = models.ManyToManyField(Tags)
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +18,9 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class TagsArticle(models.Model):
+    tag = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False)
