f21cb1b4ec9a57e8854afe4d2b6394d7e45289cd
Remezov Dmitriy <darknessdizi@gmail.com>
2023-02-16 14:36:30 +0700

    Добавлен джанго-дебагер, оптимизированы SQL запросы через prefetch_related

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -7,6 +7,6 @@ from .models import Student
 def students_list(request):
     template = 'school/students_list.html'
     ordering = 'group'
-    students_list = Student.objects.all().order_by(ordering)
+    students_list = Student.objects.all().order_by(ordering).prefetch_related('teachers')
     context = {'object_list': students_list}
     return render(request, template, context)
--- orm_migrations/website/settings.py
+++ orm_migrations/website/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'school',
+    "debug_toolbar",
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 MIDDLEWARE = [
@@ -47,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'website.urls'
