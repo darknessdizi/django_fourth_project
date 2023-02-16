0d868f99d54939eb91798d1515d34ebfd3ee447c
Remezov Dmitriy <darknessdizi@gmail.com>
2023-02-16 22:01:25 +0700

    Начало проекта с m2m, создана одна таблица

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -6,3 +6,17 @@ from .models import Article
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     pass
+
+
+# @admin.register(Tags)
+# class TagsAdmin(admin.ModelAdmin):
+#     # list_display = ['id', 'name']
+#     pass
+
+
+# @admin.register(ArticleTags)
+# class ArticleTagsAdmin(admin.ModelAdmin):
+#     # list_display = ['id', 'article', 'tags', 'is_main']
+#     pass
+
+
